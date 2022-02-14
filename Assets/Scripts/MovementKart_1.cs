e0b0155cc4911a7119b4436b5e42db740eda60a8
Kazma1129 <mariocastro1205@gmail.com>
2022-02-14 14:39:30 -0800

    movement and size

--- Assets/Scripts/MovementKart_1.cs
+++ Assets/Scripts/MovementKart_1.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using DG.Tweening;
 
 
 public class MovementKart_1 : MonoBehaviour
@@ -12,7 +13,7 @@ public class MovementKart_1 : MonoBehaviour
     public Transform kartModel;
     public Transform kartNormal;
     public Rigidbody sphere;
-    private bool accelerating = false;
+    public bool accelerating = false;
     public bool isJumping = false;
 
     private float speed, currentSpeed; // set this to private later
@@ -25,8 +26,10 @@ public class MovementKart_1 : MonoBehaviour
     public float steering = 80f;
     public float gravity = 10f;
     public LayerMask layerMask;
-    public float jumpStrength = 0;
+    public float jumpStrength = 0; // need to set this in the jump function;
     public float wheelRotationAmount = 10;
+    public float jumpCooldown;
+    public float jumpHeight;
 
     [Header("Model Parts")]
 
@@ -49,27 +52,27 @@ public class MovementKart_1 : MonoBehaviour
     {
 
         //Follow Collider // basically attachs the car to the collider
-        transform.position = sphere.transform.position - new Vector3(0, 0.4f, 0);
+        //       transform.position = sphere.transform.position - new Vector3(0, 0.4f, 0);//moved this out of the update method because it was causing some weird shaking
 
         //Accelerate
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)) { 
             speed = acceleration;
             accelerating = true;
         }
+        else { accelerating = false; }
 
         //Steer
-        if (Input.GetAxis("Horizontal") != 0)
+        if (Input.GetAxis("Horizontal") != 0) // need to find a way to jump and turn
         {
-            int dir = Input.GetAxis("Horizontal") > 0 ? 1 : -1;
-            float amount = Mathf.Abs((Input.GetAxis("Horizontal")));
+            int dir = Input.GetAxis("Horizontal") > 0 ? 1 : -1; // if value is higher than 0 then set it to 1 or -1;
+            float amount = Mathf.Abs((Input.GetAxis("Horizontal"))); 
             Steer(dir, amount);
         }
 
         if (Input.GetKeyDown(KeyCode.Space)) { 
             isJumping = true;
-            Invoke("Jump", .5f);
+            Invoke("Jump", jumpCooldown);
         }
-
 
 
 
@@ -80,7 +83,7 @@ public class MovementKart_1 : MonoBehaviour
 
 
         //b) Wheels
-        frontWheels.localEulerAngles = new Vector3(0, (Input.GetAxis("Horizontal") * wheelRotationAmount), frontWheels.localEulerAngles.z);
+        //frontWheels.localEulerAngles = new Vector3(0, (Input.GetAxis("Horizontal") * wheelRotationAmount), frontWheels.localEulerAngles.z);// moving this to fixed update to handle reverse code
         //test
 
     
@@ -94,32 +97,38 @@ public class MovementKart_1 : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //Follow Collider // basically attachs the car to the collider
+       transform.position = sphere.transform.position - new Vector3(0, 0.4f, 0);//moved this out of the update method because it was causing some weird shaking
+
         //  sphere.AddForce(-kartModel.transform.right * currentSpeed, ForceMode.Acceleration);
-        if(accelerating && Input.GetKey(KeyCode.W)) //forward
+        if (accelerating && Input.GetKey(KeyCode.W)) //forward
         { 
-           sphere.AddForce(transform.forward * currentSpeed, ForceMode.Acceleration);
-             w1.Rotate(new Vector3(-1000 * Time.deltaTime, 0, 0));
+             sphere.AddForce(transform.forward * currentSpeed, ForceMode.Acceleration);//forward
+             w1.Rotate(new Vector3(-1000 * Time.deltaTime, 0, 0));// rotation for individual wheels
              w2.Rotate(new Vector3(-1000 * Time.deltaTime, 0, 0));
              w3.Rotate(new Vector3(-1000 * Time.deltaTime, 0, 0));
              w4.Rotate(new Vector3(-1000 * Time.deltaTime, 0, 0));
+             frontWheels.localEulerAngles = new Vector3(0, (Input.GetAxis("Horizontal") * wheelRotationAmount), frontWheels.localEulerAngles.z);
         }
 
         if (accelerating && Input.GetKey(KeyCode.S)) // reverse;
         { 
-           sphere.AddForce(-transform.forward * currentSpeed, ForceMode.Acceleration);
-            w1.Rotate(new Vector3(1000 * Time.deltaTime, 0, 0));
+           sphere.AddForce(-transform.forward * currentSpeed, ForceMode.Acceleration); //reverse
+            w1.Rotate(new Vector3(1000 * Time.deltaTime, 0, 0));// rotation for individual wheels
             w2.Rotate(new Vector3(1000 * Time.deltaTime, 0, 0));
             w3.Rotate(new Vector3(1000 * Time.deltaTime, 0, 0));
             w4.Rotate(new Vector3(1000 * Time.deltaTime, 0, 0));
+            frontWheels.localEulerAngles = new Vector3(0, (Input.GetAxis("Horizontal") * -wheelRotationAmount), frontWheels.localEulerAngles.z);
         }
 
   
 
         //Gravity
-        //sphere.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
+        sphere.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
 
-        //Steering
+        //Steering  // physically rotates vehicle
         transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y + currentRotate, 0), Time.deltaTime * 5f);
+        
 
         RaycastHit hitOn;
         RaycastHit hitNear;
@@ -148,8 +157,11 @@ public class MovementKart_1 : MonoBehaviour
 
 
     public void Jump() {
-      
-            sphere.AddForce(Vector3.up * speed * Time.deltaTime);
+
+        kartModel.parent.DOComplete(); // using DOTween to make the kart jump
+        kartModel.parent.DOPunchPosition(transform.up * jumpHeight, .3f, 5, 1);
+        
         isJumping = false;
+
     }
 }
